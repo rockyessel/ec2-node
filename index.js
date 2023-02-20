6c6c3406f56e926496467afe551bec8c51585d39
Rocky Essel <rockyessel76@gmail.com>
2023-02-20 12:03:17 +0000

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 7789;
 
 app.get('/', (req, res) => {
   res.status(200).json([
